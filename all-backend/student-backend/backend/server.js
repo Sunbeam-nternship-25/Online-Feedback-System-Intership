f781356a299b189931b7cfcb835605c93057b649
Vaishnavi Dayma <vaishnavidayma3008@gmail.com>
2025-10-03 12:45:35 +0530

    get method removed

--- all-backend/student-backend/backend/server.js
+++ all-backend/student-backend/backend/server.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/", (req, res) => res.send("Server is running"));
+
 
 
 app.use((request, response, next) => {
