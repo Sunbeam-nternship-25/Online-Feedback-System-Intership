d4344addf10ec4e80818f72ed32c79425bc4c2ea
Vaishnavi Dayma <vaishnavidayma3008@gmail.com>
2025-10-03 15:28:52 +0530

    database.js updated

--- all-backend/Backend-Coco/database.js
+++ all-backend/Backend-Coco/database.js
@@ -1,16 +1,18 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 
+// Create a connection pool
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'manager',
-    database: 'feedback_system_db',
+    host: 'shuttle.proxy.rlwy.net',               // your remote MySQL host
+    user: 'root',                                 // your MySQL username
+    password: 'zoSbaTlAfxoKUTGZoCcQnfPJwPZYEwHT',// your MySQL password
+    database: 'feedback_system_db',              // your database name
+    port: 17067,                                  // your MySQL port
     connectionLimit: 10,
     maxIdle: 10,
     idleTimeout: 60000,
     queueLimit: 0,
     enableKeepAlive: true,
-    keepAliveInitialDelay:0,
-})
+    keepAliveInitialDelay: 0,
+});
 
 module.exports = pool.promise();
--- all-backend/student-backend/backend/database.js
+++ all-backend/student-backend/backend/database.js
@@ -1,19 +1,18 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
+// Create a connection pool
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "manager",
-  database: "feedback_system_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+    host: 'shuttle.proxy.rlwy.net',               // your remote MySQL host
+    user: 'root',                                 // your MySQL username
+    password: 'zoSbaTlAfxoKUTGZoCcQnfPJwPZYEwHT',// your MySQL password
+    database: 'feedback_system_db',              // your database name
+    port: 17067,                                  // your MySQL port
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
 });
 
-module.exports = {
-  pool,
-};
+module.exports = pool.promise();
