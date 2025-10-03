d61f7a4f64188f1dc3726f9f8c53b11ccfd1ab7f
Vaishnavi Dayma <vaishnavidayma3008@gmail.com>
2025-10-03 12:59:04 +0530

    config.js updated

--- frontend/app1/src/service/config.js
+++ frontend/app1/src/service/config.js
@@ -1,6 +1,6 @@
-export const config ={
-    studentServerBaseURL : 'http://localhost:4000',
-    teacherServerBaseURL : 'http://localhost:4004',
-    cocoServerBaseURL : 'http://localhost:4003',
-    adminServerBaseURL : 'http://localhost:4001',
+export const config = {
+    studentServerBaseURL : 'https://online-feedback-system-intership-1.onrender.com',
+    teacherServerBaseURL : 'https://online-feedback-system-intership-2.onrender.com',
+    cocoServerBaseURL : 'https://online-feedback-system-intership-coco.onrender.com',
+    adminServerBaseURL : 'https://online-feedback-system-intership.onrender.com',
 }
