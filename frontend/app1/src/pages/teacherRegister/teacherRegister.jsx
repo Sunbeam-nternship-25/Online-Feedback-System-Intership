f5e59ba4497e8e274bcdff52222b9b4a05938387
Vaishnavi Dayma <vaishnavidayma3008@gmail.com>
2025-10-03 20:09:24 +0530

    Teacher updated 2

--- frontend/app1/src/pages/teacherRegister/teacherRegister.jsx
+++ frontend/app1/src/pages/teacherRegister/teacherRegister.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import { registerTeacher } from '../../service/teacher';
 import Navbar from '../navbar/navbar';
 import './teacherRegister.css'; // Reuse the same CSS as login page
